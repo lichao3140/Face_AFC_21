bf73abaed7a0ca4500d4d2eee636fbee56b246b8
lichao3140 <396229938@qq.com>
2019-05-23 23:10:06 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "J3Yscp63XC1M1ut6Fk6DguUWbe9ZrnF8VyHe4KKRvw43";
+    public static final String SDK_KEY = "3bw37uNsBhbYveepprt6L4WoGrKnBgu7WQQMqnhFiJB4";
+}
